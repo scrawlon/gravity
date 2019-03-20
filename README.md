36a864ecaddecf56244f0f4205411dc7296a38e8
Duy Nguyen <andy@tier3interactive.com>
2019-03-20 18:52:11 -0400

    Added basic questions

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Command line tool for creating project folders and docker containers for easy de
 Run the commands below to install the packages. *Please note that you need the appropriate credential file to run this package.*
 
 ```
-npx install @compulse/gravity -g
+npm install @compulse/gravity -g
+
+// Enter your sites directory
 cd ~/Sites
 gravity create <Insert Project Name Here>
 ```
--- index.js
+++ index.js
@@ -2,9 +2,24 @@
 
 const chalk = require('chalk');
 const program = require('commander');
+const ora = require('ora');
+const portfinder = require('portfinder');
 
 const start = require('./lib/start');
 const files = require('./lib/files');
+const spinner = ora({
+  text: 'Testing',
+  indent: 4
+});
+
+setTimeout(() => {
+  portfinder.getPortPromise()
+    .then(port => {
+      spinner.succeed(`Port found at ${port}`);
+      console.log('\n');
+      start.gatherParameters();
+    }).catch(err => console.error(err));
+}, 1000);
 
 program
   .version('0.1.0')
@@ -16,6 +31,8 @@ program
 
 start.checkNodeVersion();
 start.initialize();
+spinner.start();
+
 
 if (program.create !== undefined) {
   if (files.directoryExists(program.create)) {
